d73414d56816329408688822c6c1d874b1d9a95d
Your Name <you@example.com>
2019-06-16 23:56:11 +0300

    first i think

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://isaacT:'+ 
+encodeURIComponent('1Q2W3E4R5T6Y') + '@cluster1-oonad.mongodb.net/clientServer?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
