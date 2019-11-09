c046fcb6c625052f8d080466b78bfc73e281f48a
isaacTadela <47701407+isaacTadela@users.noreply.github.com>
2019-11-10 00:48:44 +0200

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://isaacT:'+ encodeURIComponent('1Q2W3E4R5T6Y')+'@cluster1-oonad.mongodb.net/chevroletGarage?retryWrites=true&w=majority',{ useNewUrlParser: true ,useUnifiedTopology: true }, (err) => {
+mongoose.connect('mongodb+srv://isaacT:'+ encodeURIComponent('password')+'@cluster1-oonad.mongodb.net/chevroletGarage?retryWrites=true&w=majority',{ useNewUrlParser: true ,useUnifiedTopology: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
