06fd686903743267e14d507983ec386da988c6e9
Your Name <you@example.com>
2019-12-21 15:02:51 +0200

    update db auth

--- models/db.js
+++ models/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://isaacT:'+ encodeURIComponent('password')+'@cluster1-oonad.mongodb.net/chevroletGarage?retryWrites=true&w=majority',{ useNewUrlParser: true ,useUnifiedTopology: true }, (err) => {
+mongoose.connect('mongodb+srv://'+process.env.MONGODB_USER+':'+encodeURIComponent(process.env.MONGODB_PASS)+'@cluster1-oonad.mongodb.net/chevroletGarage?retryWrites=true&w=majority',{ useNewUrlParser: true ,useUnifiedTopology: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
-    else { console.log('Error in DB connection : ' + err) }
+    else { console.log(err) }
 });
 
 require('./treatment');
